435175a02c66752ca8f7c9c3b191b93f236b8e1a
Rishit Mandal <rishit2014@gmail.com>
2024-10-13 22:53:58 +0530

    finallll(hopefully)

--- src/components/acadhero.tsx
+++ src/components/acadhero.tsx
@@ -21,13 +21,13 @@ const HeroSection = () => {
       {/* Intro text and button */}
       <div className="relative z-10 flex items-center h-full w-[100%] px-6">
         <div className="text-left">
-          <h1 className="text-white  font-[family-name:var(--font-stardom)]  text-[12vw] md:text-[4vw] font-medium">
-            Welcome to Our Salon Academy
+          <h1 className="text-white font-[family-name:var(--font-stardom)]  text-[12vw] md:text-[5vw] font-medium">
+            Welcome to Our Academy
           </h1>
-          <p className="text-white mt-4 md:max-w-[50vw] text-[5vw] md:text-[2vw]">
+          <p className="text-white ml-3 md:max-w-[50vw] text-[5vw] md:text-[1.5vw]">
             Transform your passion into profession with our hands-on training courses.
           </p>
-          <button className="flex items-center justify-center gap-[1vw] mt-[4vw] md:px-[1.5vw] lg:px-[1vw] px-[4vw] lg:py-[0.8vw] md:py-[1vw] py-[1.8vw] bg-[#F27708] transition-all text-black md:text-[1.3vw] text-sm rounded-full hover:scale-105 hover:gap-[1.2vw]">
+          <button className="flex items-center justify-center gap-[1vw] mt-9 md:px-[1.5vw] lg:px-[1vw] px-[4vw] lg:py-[0.8vw] md:py-[1vw] py-[1.8vw] bg-[#F27708] transition-all text-black md:text-[1vw] text-sm rounded-full hover:scale-105 hover:gap-[1.2vw]">
             Learn More
             <ArrowRight size={24}/>
           </button>
