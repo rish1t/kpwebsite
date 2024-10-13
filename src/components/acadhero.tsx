2bbe630039c262b32c1cbf26cfbbee5576a69215
Rishit Mandal <rishit2014@gmail.com>
2024-10-13 22:57:45 +0530

    finallll(hopefully)

--- src/components/acadhero.tsx
+++ src/components/acadhero.tsx
@@ -24,7 +24,7 @@ const HeroSection = () => {
           <h1 className="text-white font-[family-name:var(--font-stardom)]  text-[12vw] md:text-[5vw] font-medium">
             Welcome to Our Academy
           </h1>
-          <p className="text-white ml-3 md:max-w-[50vw] text-[5vw] md:text-[1.5vw]">
+          <p className="text-white lg:ml-3 md:max-w-[50vw] text-[5vw] md:text-[1.5vw]">
             Transform your passion into profession with our hands-on training courses.
           </p>
           <button className="flex items-center justify-center gap-[1vw] mt-9 md:px-[1.5vw] lg:px-[1vw] px-[4vw] lg:py-[0.8vw] md:py-[1vw] py-[1.8vw] bg-[#F27708] transition-all text-black md:text-[1vw] text-sm rounded-full hover:scale-105 hover:gap-[1.2vw]">
